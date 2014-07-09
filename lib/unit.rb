e5a88e9eba2fe62161586cbfb5de33e924ab3087
Dhananjay Puglia <dhansiddh@gmail.com>
2014-07-09 12:17:49 +0530

    Mapping object using object id

--- lib/unit.rb
+++ lib/unit.rb
@@ -1,24 +1,39 @@
 #Convert any unit to Millimeter
-class Unit
-  METRE = Object.new
-  CM = Object.new
-  MM = Object.new
- def initialize type
-   @type = type
- end
+class Metre
 
-  def to_mm length
-    case @type
-      when Unit::METRE
-        length*1000
-      when Unit::CM
-        length*10
-      when Unit::MM
-        length
-      else
-        length
-
-    end
+  def to_mm lenght
+    lenght*1000
   end
 
 end
+class Centimetre
+
+  def to_mm lenght
+    lenght*10
+  end
+
+end
+class Millimetre
+
+  def to_mm lenght
+    lenght
+  end
+
+end
+
+class Unit
+  METRE = Metre.new
+  CM = Centimetre.new
+  MM = Millimetre.new
+def initialize (unit_type)
+
+  @unit=ObjectSpace._id2ref(unit_type.object_id)
+end
+def to_mm length
+  @unit.to_mm length
+end
+end
+
+
+
+
